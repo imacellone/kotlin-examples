653732ceb57bf03079597a7575cf8f29f2223e7e
= <italomacellone@gmail.com>
2021-04-11 23:21:54 +0200

    refactoring

--- src/main/kotlin/com/italom/kotlin/examples/reflection/JSONSerializer.kt
+++ src/main/kotlin/com/italom/kotlin/examples/reflection/JSONSerializer.kt
@@ -1,7 +1,5 @@
 package com.italom.kotlin.examples.reflection
 
-import java.math.BigDecimal
-import java.math.BigInteger
 import kotlin.reflect.full.declaredMemberProperties
 
 fun Any?.toJSON() = serialize(this, isField = false)
@@ -17,19 +15,7 @@ private fun serialize(obj: Any?, isField: Boolean = true): String = buildString 
     )
 }
 
-private fun Any.isBasicType() =
-    when (this) {
-        is Byte,
-        is Short,
-        is Int,
-        is Long,
-        is Float,
-        is Double,
-        is BigInteger,
-        is BigDecimal,
-        is Boolean -> true
-        else -> false
-    }
+private fun Any.isBasicType() = this is Number || this is Boolean
 
 private fun serializeObject(obj: Any): String = buildString {
     append("{")
